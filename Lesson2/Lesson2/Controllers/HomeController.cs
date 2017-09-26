f00609656d8816b52137e93a52cec9225cb95245
Tamara Howell <tamara.howell@hotmail.com>
2017-09-26 14:25:53 -0400

    end of lesson 3

---
+++ Lesson2/Lesson2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is the about page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Your services page.";
+
+            return View();
+        }
+    }
+}
---
+++ Lesson2/Lesson2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lesson2.Startup))]
+namespace Lesson2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
